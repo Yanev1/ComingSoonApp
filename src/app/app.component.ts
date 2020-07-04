adc17aa078a7f3d933f86989099b969bec371916
Yanev Sashko <yanev@Yanevs-MacBook-Pro.local>
2020-07-04 00:32:33 -0400

    0704 Mac Commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SubscribeService } from '../app/subscribe.service';
+
 declare const myDate: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ComingSoonApp';
+  showMsg: boolean = false;
+  subscribeData: any = <any>{};
+constructor(
+    private subscribeService: SubscribeService
+  ) { }
+  ngOnInit() {
+  }
+  subscribe(subscribeForm: NgForm) {
+    if (subscribeForm.invalid) {
+      return;
+    }
+    this.subscribeService.subscribeToList(this.subscribeData)
+      .subscribe( res => {
+      this.showMsg = true;
+      // .subscribe(res => {
+        // alert('Subscribed!');
+      this.subscribeData = {};
+      }, err => {
+        console.log('err');
+        this.showMsg = false;
+      })
+  }
   myDate = myDate()[0];
   scnds = (myDate()[0] * 86400) - myDate()[1];
-  
-  
   toSec = {
     seconds: this.scnds
   }
---
+++ src/app/subscribe.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscribeService {
+  mailChimpEndpoint = 'https://emails.us10.list-manage.com/subscribe/post-json?u=5b62dc3ff83c91582ff9da5d2&id=0d05047bfc';
+  constructor(
+    private http: HttpClient
+  ) { }
+  subscribeToList(data) {
+    const params = new HttpParams()
+      .set('EMAIL', data.email) //EMAIL is the property in Mailchimp and it has to be on all "input name" tags as well
+      .set('group[15357][1]', 'true')
+      // .set('b_b736eb9e9077236cbd681a53b_4b66a82360', '');
+      .set('b_5b62dc3ff83c91582ff9da5d2_0d05047bfc', '');
+    const mailChimpUrl = `${this.mailChimpEndpoint}&${params.toString()}`;
+    return this.http.jsonp(mailChimpUrl, 'c')
+  }
+  
+}
